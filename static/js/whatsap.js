b762316f30081fd769722e1c1bbf2bf5ca60d0c2
emerson <xinayespinoza@gmail.com>
2023-10-29 18:26:28 -0300

    fix whatsap

--- static/js/whatsap.js
+++ static/js/whatsap.js
@@ -1,6 +1,8 @@
-document.getElementById("send-whatsapp").addEventListener("click", function () {
-  var message = document.getElementById("whatsapp-message").value;
-  var encodedMessage = encodeURIComponent(message);
-  var whatsappURL = "https://api.whatsapp.com/send?phone=+51918151231&text=" + encodedMessage;
-  window.open(whatsappURL, "_blank");
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("send-whatsapp").addEventListener("click", function () {
+    var message = document.getElementById("whatsapp-message").value;
+    var encodedMessage = encodeURIComponent(message);
+    var whatsappURL = "https://api.whatsapp.com/send?phone=+51918151231&text=" + encodedMessage;
+    window.open(whatsappURL, "_blank");
+  });
 });
